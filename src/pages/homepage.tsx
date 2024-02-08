9e87c5b55e94f8c006b557ac84b0e909c9afdb5f
Zhang YiYang <120240967+LeonardoSya@users.noreply.github.com>
2024-02-08 22:28:33 +0800

    modified:   package.json 	modified:   pnpm-lock.yaml 	new file:   src/components/ui/separator.tsx 	modified:   src/pages/homepage.tsx

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import {Separator} from '@/components/ui/separator';
 
 const Homepage = () => {
     return (
         <div>
-            homepage
+            <div className="space-y-1">
+                <h4 className="text-sm font-medium leading-none">Radix Primitives</h4>
+                <p></p>
+            </div>
             <Outlet />
         </div>
     )
